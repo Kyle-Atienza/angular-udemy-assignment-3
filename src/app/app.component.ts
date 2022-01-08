6b962edade4d78c931a6afa5db460557a163a3fc
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-01-09 00:57:42 +0800

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+    .hide{
+      display: none;
+    }
+    .blue{
+      background: blue;
+    }
+  `]
 })
 export class AppComponent {
   title = 'angular-udemy-assignment-3';
+  displayContent = false;
+  count = 0
+  clicks : number[] = []
+
+  toggleDisplay() {
+    this.count++
+    this.clicks.push(this.count)
+    console.log(this.clicks)
+    if (this.displayContent){
+      this.displayContent = false
+    } else {
+      this.displayContent = true
+    }
+  }
 }
